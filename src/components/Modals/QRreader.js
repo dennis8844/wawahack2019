922d53417153bd6e823bce802e7c640ae61d0ab0
dennis8844 <dpitcock@gmail.com>
2019-10-12 13:01:59 -0400

    qrs working in modal

--- src/components/Modals/QRreader.js
+++ src/components/Modals/QRreader.js
@@ -3,7 +3,10 @@ import QrReader from 'react-qr-reader';
 import QRCode from 'qrcode.react';
 import GridContainer from "../Grid/GridContainer";
 import GridItem from "../Grid/GridItem";
+import Button from '@material-ui/core/Button';
 import rawKiosk from "../../utils/kiosk.json";
+import AddIcon from '@material-ui/icons/Add';
+import SmartphoneIcon from '@material-ui/icons/Smartphone';
 
 class QRreader extends Component {
     constructor(props){
@@ -12,7 +15,8 @@ class QRreader extends Component {
             delay: 300,
             result: '',
             scanned: false,
-            activated: false
+            activated: false,
+            addMode: 'all'
         }
 
         this.handleScan = this.handleScan.bind(this)
@@ -27,13 +31,20 @@ class QRreader extends Component {
     handleError(err){
         console.error(err)
     }
+
+    setAddMode(e) {
+        if (e) {
+            this.setState({ addMode : this.state.addMode === 'all' ? 'single' : 'all' });
+        }
+    }
+
     render(){
         console.log(this.props);
         const kioskData = JSON.stringify(rawKiosk);
         const previewStyle = {
-            height: 320,
-            width: 320,
-            margin: 'auto'
+            height: 240,
+            width: 240,
+            margin: this.props.source === 'receipt' ? 'auto' : ''
         }
 
         const titleText = this.props.source === 'receipt' ? 'Scan from a reciept' : 'Scan the QR in your app';
@@ -44,17 +55,29 @@ class QRreader extends Component {
                 <h3>To Quickly Add Your Items</h3>
                 <GridContainer direction='row' justify="center">
                     {this.props.source === 'receipt' || this.state.activated ?
-                    <GridItem style={{display: 'flex', flexDirection: 'column'}}>
-                        <QrReader
-                            delay={this.state.delay}
-                            style={previewStyle}
-                            onError={this.handleError}
-                            onScan={this.handleScan}
-                        />
-                        <p>{this.state.result && this.state.result}</p>
-                    </GridItem> :
                         <GridItem style={{display: 'flex', flexDirection: 'column'}}>
-                            <QRCode value={kioskData} style={previewStyle} />
+                            <QrReader
+                                delay={this.state.delay}
+                                style={previewStyle}
+                                onError={this.handleError}
+                                onScan={this.handleScan}
+                            />
+                            <p>{this.state.result && this.state.result}</p>
+                        </GridItem> :
+                        <GridItem style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                            <Button variant="contained" size="large" variant="contained" size="large"
+                                    onClick={(e) => {this.setAddMode(e)}} >
+                                {this.state.addMode === 'all' ? <AddIcon /> : <SmartphoneIcon />}
+                                <div>{this.state.addMode === 'all' ? 'Add Item from App' : 'View Your Items Here'}</div>
+                            </Button>
+                            {this.state.addMode === 'all' ? <QRCode value={kioskData} style={previewStyle}/> :
+                                <QrReader
+                                    delay={this.state.delay}
+                                    style={previewStyle}
+                                    onError={this.handleError}
+                                    onScan={this.handleScan}
+                                />
+                            }
                         </GridItem>
                     }
                 </GridContainer>
